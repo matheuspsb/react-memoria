9a9b01384f747014fc7ca7e0521f57b33c5d870d
Matheus <matheusslg1@gmail.com>
2022-06-13 09:50:14 -0300

    feat: styling container

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,21 +1,30 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-
+    width: 100%;
+    max-width: 750px;
+    margin: auto;
+    display: flex;
+    padding: 50px 0;
 `;
 
 export const Info = styled.div`
-
+    display: flex;
+    flex-direction: column;
+    width: auto;
 `;
 
 export const LogoLink = styled.a`
-
+    display: block;
 `;
 
 export const InfoArea = styled.div`
-
+    width: 100%;
+    margin: 10px 0;
 `;
 
 export const GridArea = styled.div`
-
+    flex: 1;
+    display: flex;
+    justify-content: flex-end;
 `;
