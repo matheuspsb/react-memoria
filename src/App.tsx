f620278580dafd915174f126f25fcb4d8e5953d6
Matheus <matheusslg1@gmail.com>
2022-06-13 11:06:35 -0300

    feat: Creating info component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 import * as C from './App.styles';
+import { InfoItem } from './components/InfoItem';
 import logoImage from './assets/devmemory_logo.png'
 
 const App = () => {
   return (
     <C.Container>
       <C.Info>
+        
         <C.LogoLink href="">
           <img src={logoImage} width="200" alt="" />
         </C.LogoLink>
 
         <C.InfoArea>
-          ...
+          <InfoItem label='Tempo' value='00:00' />
+          <InfoItem label='Movimentos' value='0' />
         </C.InfoArea>
 
         <button>Reiniciar</button>
+     
       </C.Info>
       <C.GridArea>
         ...
